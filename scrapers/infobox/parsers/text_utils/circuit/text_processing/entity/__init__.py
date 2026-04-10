f1870abd0d0af8b003adda5657e94154d9b55a26
Bartosz Gembalczyk <bart.polska@gmail.com>
2026-04-10 22:41:00 +0200

    poprawki w importach

---
+++ scrapers/infobox/parsers/text_utils/circuit/text_processing/entity/__init__.py
@@ -0,0 +1,4 @@
+from scrapers.infobox.parsers.text_utils.circuit.text_processing.entity.base import CircuitEntityParser
+
+__all__ = ["CircuitEntityParser"]
+
--- scrapers/infobox/parsers/text_utils/circuit/text_processing/entity/additional_info.py
+++ scrapers/infobox/parsers/text_utils/circuit/text_processing/entity/additional_info.py
@@ -3,7 +3,7 @@ from typing import Any
 
 from scrapers.helpers.text_normalization import clean_infobox_text
 from scrapers.helpers.text_normalization import split_delimited_text
-from scrapers.infobox.parsers.text_utils.circuit.text_processing.entity.base import CircuitEntityParser
+from scrapers.infobox.parsers.text_utils.circuit.text_processing.entity import CircuitEntityParser
 
 if TYPE_CHECKING:
     from models.records.link import LinkRecord
