8435832675d279e1106a1ba2c82ea73c82645cd1
Bartosz Gembalczyk <bart.polska@gmail.com>
2026-04-10 22:38:54 +0200

    precommit

--- scrapers/section/selection_strategy/__init__.py
+++ scrapers/section/selection_strategy/__init__.py
@@ -1,5 +1,7 @@
 from scrapers.section.selection_strategy.base import SectionSelectionStrategy
-from scrapers.section.selection_strategy.wikipedia_by_id import WikipediaSectionByIdSelectionStrategy
+from scrapers.section.selection_strategy.wikipedia_by_id import (
+    WikipediaSectionByIdSelectionStrategy,
+)
 
 __all__ = [
     "WikipediaSectionByIdSelectionStrategy",
