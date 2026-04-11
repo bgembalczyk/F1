b90342d938505844384da9394e744cb0a60d997e
Bartosz Gembalczyk <bart.polska@gmail.com>
2026-04-11 21:54:47 +0200

    poprawki w importach oraz sprzątanie

--- scrapers/parsers/wiki/element.py
+++ scrapers/parsers/wiki/element.py
@@ -18,7 +18,6 @@ from scrapers.parsers.wiki.paragraph import WikiParagraphParser
 from scrapers.parsers.infobox.wiki_html import WikiInfoboxHtmlParser
 from scrapers.parsers.rules import ParserRule
 from scrapers.parsers.table.wiki.table import WikiTableHtmlParser
-from scrapers.parsers.wiki.list import ListElementParser
 from scrapers.parsers.wiki.references_wrap import ReferencesWrapParser
 from scrapers.parsers.wiki.table import WikiTableParser
 
