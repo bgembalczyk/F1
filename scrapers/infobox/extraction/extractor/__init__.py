3fc336aef94660e7caa2d6efcfdb543fd7629c68
Bartosz Gembalczyk <bart.polska@gmail.com>
2026-04-11 17:58:52 +0200

    poprawki w importach oraz sprzątanie

--- scrapers/infobox/extraction/extractor/__init__.py
+++ scrapers/infobox/extraction/extractor/__init__.py
@@ -3,11 +3,9 @@ from scrapers.infobox.extraction.extractor.circuit_extractor import (
     CircuitInfoboxExtractor,
 )
 from scrapers.infobox.extraction.extractor.link import InfoboxLinkExtractor
-from scrapers.infobox.extraction.extractor.infobox_extractor import InfoboxExtractorProtocol
 
 __all__ = [
     "InfoboxLinkExtractor",
     "BaseInfoboxExtractor",
     "CircuitInfoboxExtractor",
-    "InfoboxExtractorProtocol",
 ]
